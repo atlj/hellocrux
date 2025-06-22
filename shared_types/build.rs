d133bd10c0bd784628dd42b99f481252254deb1b
Burak Güner <burak.guner@callstack.com>
2025-06-22 12:23:32 +0300

    don't generate typescript types

--- shared_types/build.rs
+++ shared_types/build.rs
@@ -6,16 +6,11 @@ fn main() -> anyhow::Result<()> {
     println!("cargo:rerun-if-changed=../shared");
 
     let mut typegen = TypeGen::new();
-
     typegen.register_app::<CounterApp>()?;
 
     let output_root = PathBuf::from("./generated");
-
     typegen.swift("SharedTypes", output_root.join("swift"))?;
-
     typegen.java("com.crux.example.simple_counter", output_root.join("java"))?;
-
-    typegen.typescript("shared_types", output_root.join("typescript"))?;
 
     Ok(())
 }
