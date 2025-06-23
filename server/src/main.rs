7664ad42a93178bbc019514b4b571b8877812ac6
Burak Güner <burak.guner@callstack.com>
2025-06-23 22:52:35 +0300

    basic http server

---
+++ server/src/main.rs
@@ -0,0 +1,25 @@
+use axum::{Json, Router, routing::get};
+use domain::Movie;
+use tokio::{main, net::TcpListener};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/get_movies", get(movie_list_handler));
+
+    let listener = TcpListener::bind("localhost:3000")
+        .await
+        .expect("Server can't be started");
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn movie_list_handler() -> Json<Vec<Movie>> {
+    Json(
+        vec![
+            Movie {
+                id:"my-id".to_string(),
+                thumbnail: "https://m.media-amazon.com/images/M/MV5BMTkzMzM3OTM2Ml5BMl5BanBnXkFtZTgwMDM0NDU3MjI@._V1_FMjpg_UY2048_.jpg".to_string()
+            }
+        ]
+        )
+}
