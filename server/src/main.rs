b0d738376ba92a72e2aa9b508065fb83be169c98
Burak Güner <burak.guner@callstack.com>
2025-07-05 00:48:33 +0300

    add logging to server

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,6 +3,7 @@ use std::sync::Arc;
 use axum::{Json, Router, extract, routing::get};
 use clap::Parser;
 use domain::Media;
+use log::info;
 use server::{Args, media::get_media_items};
 use tokio::net::TcpListener;
 
@@ -15,11 +16,14 @@ type State = extract::State<Arc<AppState>>;
 
 #[tokio::main]
 async fn main() {
+    env_logger::init();
     let args = Args::parse();
 
-    println!("Crawling media items");
+    info!("Crawling media items");
 
     let entries = get_media_items(args.media_dir.clone()).await;
+
+    info!("Found {:#?} media items", entries.len());
 
     let shared_state = Arc::new(AppState { args, entries });
 
@@ -31,6 +35,8 @@ async fn main() {
     let listener = TcpListener::bind("0.0.0.0:3000")
         .await
         .expect("Server can't be started");
+
+    info!("Starting the server");
 
     axum::serve(listener, app).await.unwrap();
 }
