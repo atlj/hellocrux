7726bbc769ef45d6472655b94decee4c9cfaa595
Burak Güner <burak.guner@callstack.com>
2025-07-27 02:35:52 +0300

    retain the player object if the url is the same

--- ios/Streamy/Screens/Player/Player.swift
+++ ios/Streamy/Screens/Player/Player.swift
@@ -2,8 +2,38 @@ import AVKit
 import SwiftUI
 
 struct Player: UIViewControllerRepresentable {
-    var player: AVPlayer
-    var onProgress: (_ seconds: UInt64) -> Void
+    var url: URL
+    var initialSeconds: UInt64?
+    var onProgress: ((CMTime) -> Void)?
+
+    private static var sharedPlayer: AVPlayer?
+    private static var sharedPlayerUrl: URL?
+
+    private var player: AVPlayer {
+        if let sharedPlayer = Player.sharedPlayer, Player.sharedPlayerUrl == url {
+            return sharedPlayer
+        }
+
+        let player = AVPlayer(url: url)
+        Player.sharedPlayerUrl = url
+        Player.sharedPlayer = player
+
+        if let initialSeconds {
+            player.seek(to: .init(seconds: Double(initialSeconds), preferredTimescale: CMTimeScale(NSEC_PER_SEC)))
+        }
+
+        player.addPeriodicTimeObserver(forInterval: .init(seconds: 1, preferredTimescale: CMTimeScale(NSEC_PER_SEC)), queue: .main) { time in
+            if time.seconds.rounded(.towardZero) == 0 {
+                return
+            }
+
+            onProgress?(time)
+        }
+
+        player.play()
+
+        return player
+    }
 
     func makeUIViewController(context _: Context) -> AVPlayerViewController {
         let viewController = AVPlayerViewController()
@@ -22,6 +52,8 @@ struct Player: UIViewControllerRepresentable {
     static func dismantleUIViewController(_ uiViewController: AVPlayerViewController, coordinator _: ()) {
         uiViewController.player?.replaceCurrentItem(with: nil)
         uiViewController.player = nil
+        Player.sharedPlayer = nil
+        Player.sharedPlayerUrl = nil
     }
 
     typealias UIViewControllerType = AVPlayerViewController
--- ios/Streamy/Screens/Player/PlayerScreen.swift
+++ ios/Streamy/Screens/Player/PlayerScreen.swift
@@ -4,40 +4,16 @@ import SwiftUI
 
 struct PlayerScreen: View {
     let url: URL
-    private let player: AVPlayer
     let itemId: String
     let episode: Episode?
-
-    init(url: URL, itemId: String, episode: Episode? = nil, initialSeconds: UInt64?) {
-        self.url = url
-        player = AVPlayer(url: url)
-        player.play()
-
-        if let initialSeconds {
-            player.seek(to: .init(seconds: Double(initialSeconds), preferredTimescale: CMTimeScale(NSEC_PER_SEC)))
-        }
-
-        self.itemId = itemId
-        self.episode = episode
-
-        player.addPeriodicTimeObserver(forInterval: .init(seconds: 1, preferredTimescale: CMTimeScale(NSEC_PER_SEC)), queue: .main) { time in
-            if time.seconds.rounded(.towardZero) == 0 {
-                return
-            }
-
-            Core.shared.update(.playbackProgress(.init(id: itemId, episode: episode, progress_seconds: UInt64(time.seconds))))
-        }
-    }
+    let initialSeconds: UInt64?
 
     @EnvironmentObject var core: Core
 
     var body: some View {
-        Player(player: player) { seconds in
-            if seconds == 0 {
-                return
-            }
+        Player(url: url, initialSeconds: initialSeconds) { time in
 
-            core.update(.playbackProgress(.init(id: itemId, episode: episode, progress_seconds: seconds)))
+            Core.shared.update(.playbackProgress(.init(id: itemId, episode: episode, progress_seconds: UInt64(time.seconds))))
         }
     }
 }
@@ -45,7 +21,7 @@ struct PlayerScreen: View {
 #Preview {
     PlayerScreen(
         url: URL(string: "http://localhost:3000/static/jaho/recording.mov")!,
-        itemId: "1", initialSeconds: nil
+        itemId: "1", episode: nil, initialSeconds: nil
     )
     .environmentObject(Core())
 }
