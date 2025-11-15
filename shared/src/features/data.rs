9253a912121e7f73a8f1aa128faba9a0f051eb80
Burak Güner <burak.guner@callstack.com>
2025-11-15 23:10:02 +0300

    try guessing episode identifier

--- shared/src/features/data.rs
+++ shared/src/features/data.rs
@@ -80,26 +80,24 @@ pub fn update_data(model: &mut Model, request: DataRequest) -> Command<Effect, E
 
             match http::get(url).into_future(ctx.clone()).await {
                 http::HttpOutput::Success { data, .. } => {
-                    let files = data
-                        .and_then(|data| serde_json::from_str::<Vec<String>>(&data).ok())
-                        .map(|files| {
-                            let len = files.len();
-                            let map = files.into_iter().fold(
-                                HashMap::with_capacity(len),
-                                |mut map, file| {
-                                    map.insert(
-                                        file,
-                                        // TODO implement auto detection
-                                        EpisodeIdentifier {
-                                            season_no: 0,
-                                            episode_no: 0,
-                                        },
-                                    );
-                                    map
-                                },
-                            );
-                            (id, map)
-                        });
+                    let files =
+                        data.and_then(|data| serde_json::from_str::<Vec<String>>(&data).ok())
+                            .map(|files| {
+                                let len = files.len();
+                                let map = files.into_iter().fold(
+                                    HashMap::with_capacity(len),
+                                    |mut map, file| {
+                                        let identifier = detect_episode_identifier(&file)
+                                            .unwrap_or(EpisodeIdentifier {
+                                                season_no: 1,
+                                                episode_no: 1,
+                                            });
+                                        map.insert(file, identifier);
+                                        map
+                                    },
+                                );
+                                (id, map)
+                            });
 
                     update_model(
                         &ctx,
@@ -209,5 +207,40 @@ pub fn update_data(model: &mut Model, request: DataRequest) -> Command<Effect, E
                 .into_future(ctx.clone())
                 .await;
         }),
+    }
+}
+
+fn detect_episode_identifier(path: &str) -> Option<EpisodeIdentifier> {
+    let re = regex::Regex::new(r".*S([0-9]+)E([0-9]+).*").expect("Invalid regex supplied");
+    let first_capture = re.captures_iter(path).next()?;
+    let (_, [season_no_str, episode_no_str]) = first_capture.extract();
+
+    Some(EpisodeIdentifier {
+        season_no: season_no_str.parse().ok()?,
+        episode_no: episode_no_str.parse().ok()?,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use domain::series::EpisodeIdentifier;
+
+    use crate::features::data::detect_episode_identifier;
+
+    #[test]
+    fn test_detect_episode_identifier() {
+        assert_eq!(
+            detect_episode_identifier("my-series.S02E01.1080p.x265-HEYYY.mkv").unwrap(),
+            EpisodeIdentifier {
+                season_no: 2,
+                episode_no: 1
+            }
+        );
+
+        assert!(detect_episode_identifier("my-series.S02E.1080p.x265-HEYYY.mkv").is_none());
+        assert!(detect_episode_identifier("my-series.S02.1080p.x265-HEYYY.mkv").is_none());
+        assert!(detect_episode_identifier("my-series.02.1080p.x265-HEYYY.mkv").is_none());
+        assert!(detect_episode_identifier("my-series.E2S7.1080p.x265-HEYYY.mkv").is_none());
+        assert!(detect_episode_identifier("my-series.SE.1080p.x265-HEYYY.mkv").is_none());
     }
 }
